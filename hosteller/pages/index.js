6edb9b12a27db2a5cf53abca457020e926dd9044
PRANSU YADAV <71815813+Pransu-code@users.noreply.github.com>
2023-07-27 15:43:22 +0545

    all changes till july

--- hosteller/pages/index.js
+++ hosteller/pages/index.js
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Layout from "../components/Layout";
 
 export default function Home() {
